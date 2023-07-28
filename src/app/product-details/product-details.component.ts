91f13f1a912547a5b49706e7d9a8532ecf368cb7
Martin Petrov <martuto31@gmail.com>
2023-07-28 18:42:23 +0300

    Product details updated to work with db.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Swiper from 'swiper';
+import { ProductService } from '../Services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -8,10 +11,18 @@ import Swiper from 'swiper';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getProductById(this.productId);
   }
+
+  productId: number = this.route.snapshot.params['id'];
+  product: Product = {id: 0, name: '', price: 0, description: '', lightIntensity: 0, growDifficulty: 0, productType: 0, 
+                      petCompatibility: false, airPurify: false, picturesData: [], productSizes: [], productColors: []};
+  currentPictureIndex: number = 0;
+  // cart: Cart = { products: [] };
+
 
   ngAfterViewInit() {
     new Swiper('.swiper-container', {
@@ -22,5 +33,19 @@ export class ProductDetailsComponent implements OnInit {
         clickable: true,
       },
     });
+  }
+
+  getProductById(id: number): any{
+    this.productService.getProductById(id).subscribe((product: Product) =>{
+      this.product = product;
+    });
+  }
+
+  getBase64ImageUrl(base64String: string): string {
+    return `data:image/jpeg;base64,${base64String}`;
+  }
+
+  selectPicture(index: number){
+    this.currentPictureIndex = index;
   }
 }
