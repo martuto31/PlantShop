fc5439323f605547bd4dd924aed149c12dfa4815
Martin Petrov <martuto31@gmail.com>
2023-07-29 16:06:22 +0300

    Implemented cart component functionallity.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cart } from '../models/cart';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router){}
+
+  cart: Cart = { products: []}
+  totalPrice: number = 0;
+  isOrderPlaced: boolean = false;
 
   ngOnInit(): void {
+    const cart = localStorage.getItem('cart');
+    
+    if(cart != null){
+      this.cart = cart ? JSON.parse(cart) : [];
+    }
+
+    this.totalPrice = this.calculateTotalPriceFromCart();
   }
 
+  getBase64ImageUrl(base64String: string): string {
+    return `data:image/jpeg;base64,${base64String}`;
+  }
+
+  removeFromCart(id: number){
+    const index = this.cart.products.findIndex((product) => product.id === id);
+
+    this.cart.products.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(this.cart))
+
+    this.totalPrice = this.calculateTotalPriceFromCart();
+  }
+
+  calculateTotalPriceFromCart(){
+    var totalPrice = 0;
+    this.cart.products.forEach(product => {
+      totalPrice += product.price;
+    });
+    return totalPrice;
+  }
+
+  private clearCart(){
+    localStorage.removeItem('cart');
+  }
 }
