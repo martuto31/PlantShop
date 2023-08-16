8cd25d220f65b96e8b74cfe70e358c3f07490b95
Martin Petrov <martuto31@gmail.com>
2023-08-17 02:48:54 +0300

    Favourite component updated to retrieve products from localstorage if user isnt logged in.

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../models/product';
 import { ProductService } from '../Services/product.service';
 import { Router } from '@angular/router';
 import { Cart } from '../models/cart';
+import { UserService } from '../Services/user.service';
 
 @Component({
   selector: 'app-favourite',
@@ -13,11 +14,16 @@ export class FavouriteComponent implements OnInit {
 
   products: Product[] = [];
   cart: Cart = { products: [] };
+  private isAuthenticated: boolean = false;
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private productService: ProductService, private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
-    this.getAllFavouriteProducts();
+    this.userService.isAuthenticated$.subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+
+    this.getAllFavouriteProducts()
     
     const storedCart = localStorage.getItem('cart');
     if(storedCart != null){
@@ -26,11 +32,22 @@ export class FavouriteComponent implements OnInit {
   }
 
   getAllFavouriteProducts(){
-    this.productService.getAllUserFavouriteProducts().subscribe((products: Product[]) => {
-      products.forEach((product: Product) => {
-        this.products.push(product);
-      });
-    })
+    if(this.isAuthenticated === false){
+      this.productService.getAllUserFavouriteProducts().subscribe((products: Product[]) => {
+        products.forEach((product: Product) => {
+          this.products.push(product);
+        });
+      })
+    }
+    else{
+      const productsJSON = localStorage.getItem('favouriteProducts');
+      if(productsJSON){
+        this.products = JSON.parse(productsJSON);
+      }
+      else{
+        this.products = [];
+      }
+    }
   }
 
   getBase64ImageUrl(base64String: string): string {
